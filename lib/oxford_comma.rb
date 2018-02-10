2c9e4517c1a091c4b023a6f3499fd9b5f7fe1668
Matthew McDonald <matthew.mcd1031@gmail.com>
2018-02-10 15:03:02 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  if array.length < 2
+    array.join
+  elsif array.length == 2
+    array.insert(1, "and")
+    array.join(" ")
+  else array.length > 2
+    str = ", and " + array.pop
+    array.join(", ") << str
+    end
 end
