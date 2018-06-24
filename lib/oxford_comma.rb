aa2ebe16102df4b1c294fd818527cbfcd6c9b4aa
Matthew McDonald <matthew.mcd1031@gmail.com>
2018-06-24 19:48:51 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,5 +2,5 @@ def oxford_comma(array)
 string = array.join(", and ")
 new_array = []
 new_array << string
-new_array.delete_if{|x| new_array.include?("and")}
+new_array.split.delete_if{|x| new_array.include?("and")}
 end
