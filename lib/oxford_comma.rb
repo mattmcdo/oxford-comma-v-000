32067dce121682b6fdb015ce8e650f79c9b8e309
Matthew McDonald <matthew.mcd1031@gmail.com>
2018-06-24 20:26:36 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+if array.length == 1
+  "#{array[0]}"
+elsif array.length == 2
+  "#{array[0]} and #{array[1]}"
+elsif array.length >= 3
+  new_last_array_item = "and #{array[-1]}"
+  array.pop
+  array.push(new_last_array_item)
+  array.join(", ")
+end
 end
